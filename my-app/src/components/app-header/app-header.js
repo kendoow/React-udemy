e41eaa34c362a8bfafcf5835ac20ecb683e8a134
kendo <kendo_ow@mail.ru>
2021-03-14 12:52:45 +0300

    ready

---
+++ my-app/src/components/app-header/app-header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './app-header.css';
+
+import styled from 'styled-components';
+
+const Header = styled.div`
+    display:flex;   
+    align-items: flex-end;
+    justify-content: space-between;
+    h1 {
+        font-size: 26px;
+        color: ${props => props.colored ? 'black' :'red' };
+        :hover{
+            color: blue;
+        }
+    }
+    h2{
+        font-size: 1.2rem;
+        color: grey;
+        :hover{
+            opacity:.9;
+        }
+    }
+`
+
+const AppHeader = ({liked, allPosts}) =>{
+    return(
+        <Header colored as='a'>
+            <h1>Vadim Khidirov</h1>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
+        </Header>
+    )
+}
+
+export default AppHeader;
